a080183b6d79151cabe43e04cfc76c49fba9a15e
BarakShachar <barakon111@gmail.com>
2022-11-28 19:02:23 +0200

    add implementation in build.gradle add register to db when register to the app

--- app/src/main/java/com/example/calories_calculator/Register.java
+++ app/src/main/java/com/example/calories_calculator/Register.java
@@ -1,10 +1,13 @@
 package com.example.calories_calculator;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -13,9 +16,15 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
     EditText usernameEditText, emailEditText, passwordEditText,
@@ -23,6 +32,7 @@ public class Register extends AppCompatActivity {
     Button register_button;
     ProgressBar progressBar;
     Create_user user;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +53,13 @@ public class Register extends AppCompatActivity {
         String username = usernameEditText.getText().toString();
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
-        int height = Integer.parseInt(heightEditText.getText().toString());
-        int weight = Integer.parseInt(weightEditText.getText().toString());
+//        int height = Integer.parseInt(heightEditText.getText().toString());
+//        int weight = Integer.parseInt(weightEditText.getText().toString());
 
-        boolean isValid = validateData(username,email,password);
+        boolean isValid = validateData(username,email, password);
         if(!isValid){ return;}
 
-        createAccountInFirebase(email,password);
+        createAccountInFirebase(username, email,password);
 
 
     }
@@ -72,7 +82,7 @@ public class Register extends AppCompatActivity {
           return true;
     }
 
-    void createAccountInFirebase(String email, String password){
+    void createAccountInFirebase(String username, String email, String password){
         changeInProgress(true);
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
@@ -80,14 +90,37 @@ public class Register extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 changeInProgress(false);
                 if(task.isSuccessful()){ //creating account is done
-                  Toast.makeText(Register.this, "Account created successfully!", Toast.LENGTH_SHORT).show();
                   firebaseAuth.signOut();
+                  createAccountInFirebaseDB(username, email);
+                  Toast.makeText(Register.this, "Account created successfully!", Toast.LENGTH_SHORT).show();
                   main_screen();
               } else{ // failure while creating the account
                   Toast.makeText(Register.this,task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    void createAccountInFirebaseDB(String username, String email){
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", username);
+        user.put("is_admin", false);
+        user.put("admin_mail", null);
+        user.put("menus", new HashMap<>());
+        db.collection("users").document(email)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("main_activity", "user successfully written to DB!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("main_activity", "Error writing user document", e);
+                    }
+                });
     }
 
     void changeInProgress(boolean inProgress){
